0591e11fe138a1c69f3310396fae12a38127e481
Mohamed Hamed <mohamed.hamed1@vodafone.com>
2017-08-22 11:36:50 +1200

    constractor example

--- src/components/app.js
+++ src/components/app.js
@@ -1,36 +1,50 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  // static displayName = 'something else'
-  // static defaultProps= {
-  //   importantArray:[1,2,3]
-  // // }
+  static displayName = 'something else'
+  static defaultProps = {
+    importantArray: [1, 2, 3]
+  }
   // if you want to pass props to component from outside
-  // constructor(props){
-  //   super(props)
-  //this.state={
-    // anotherWayToSetState:''
-  //}
-  // }
+  constructor(props) {
+    super(props);
+    //on way to set initial state
+    this.state = {
+      anotherWayToSetState: ''
+    };
+    // if not using arrow function pass the component object to the function
+    this.oneFunction = this.oneFunction.bind(this);
+  }
+  // // another way to set initial state
   state ={
     ourInitialState:'yallow'
+  };
+  oneFunction() {
+    console.log('oneFunction');
+    console.log(this.props);
+  }
+  useArrows = () => {
+    console.log('useArrows works without binding');
+    console.log(this.props);
   }
   render() {
     console.log(this.state);
     return (
-      <h1>
-        test
-      </h1>
+      <div>
+        <button onClick={this.oneFunction}>One function</button>
+        <button onClick={this.useArrows}>Arrow function</button>
+
+      </div>
     );
   }
 }
 
-function myTestWrapper(WrappedComponent){
-  return class extends Component{
+function myTestWrapper(WrappedComponent) {
+  return class extends Component {
     render() {
       return (
-        <div style={{ backgroundColor: 'blue' }}>
-          <WrappedComponent/>
+        <div>
+          <WrappedComponent />
         </div>
       );
     }
