4d070031bd6bb6573b08b04f819843f93347a959
Mohamed Hamed <mohamed.hamed1@vodafone.com>
2017-08-22 14:01:15 +1200

    component did mount example

--- src/components/app.js
+++ src/components/app.js
@@ -1,59 +1,29 @@
 import React, { Component } from 'react';
+import loggify from './loggify';
 
 class App extends Component {
-  static displayName = 'something else'
-  static defaultProps = {
-    importantArray: [1, 2, 3]
+  fetchData = () => {
+    console.log('going to fetch data');
+    setTimeout(() => {
+      console.log('data recived');
+      this.setState({
+        data: Math.random()
+      });
+    }, 1500);
   }
-  // if you want to pass props to component from outside
-  constructor(props) {
-    super(props);
-    //on way to set initial state
-    this.state = {
-      anotherWayToSetState: ''
-    };
-    // if not using arrow function pass the component object to the function
-    this.oneFunction = this.oneFunction.bind(this);
-  }
-  // // another way to set initial state
-  state ={
-    ourInitialState:'yallow'
-  };
-  oneFunction() {
-    console.log('oneFunction');
-    console.log(this.props);
-  }
-  useArrows = () => {
-    console.log('useArrows works without binding');
-    console.log(this.props);
+  componentDidMount() {
+    this.fetchData();
   }
   render() {
-    console.log(this.state);
+
     return (
       <div>
-        <button onClick={this.oneFunction}>One function</button>
-        <button onClick={this.useArrows}>Arrow function</button>
-
+        Hello
       </div>
     );
   }
 }
 
-function myTestWrapper(WrappedComponent) {
-  return class extends Component {
-    render() {
-      return (
-        <div>
-          <WrappedComponent />
-        </div>
-      );
-    }
-  };
-}
+App = loggify(App);
 
-App = myTestWrapper(App);
-// app.defaultProps= {
-//   importantArray:[4,5,6]
-// }
-// App.displayName = 'haveFunn';
 export default App;
